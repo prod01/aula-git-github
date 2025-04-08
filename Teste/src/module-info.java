50b9f951d10a9db26607bcbf536babfdc2badb37
O Pedro Rodrigues <98363013+prod01@users.noreply.github.com>
2025-04-07 21:25:50 -0300

    Update module-info.java

--- Teste/src/module-info.java
+++ Teste/src/module-info.java
@@ -1,9 +1,7 @@
-/**
- * 
- */
-/**
- * @author Pedro Rodrigues
- *
- */
-module Teste {
+package teste;
+public class Teste {
+public static void main(String[] args) {
+int num1 = 3, num2 = 5, soma;
+soma = num1 + num2;
+}
 }
